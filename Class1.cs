bdfd5c862c4a158477a03acaecff6425dc2e01c3
fishman123456 <den9152@yandex.ru>
2023-01-15 18:53:25 +0700

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace autokad_test_dll_15_01_2023
+{
+    public class Class1
+    {
+        [CommandMethod ("Demo")]
+        public void Demo()
+        {
+            Form1 mf = new Form1();
+            mf.Show();
+        }
+    }
+}
