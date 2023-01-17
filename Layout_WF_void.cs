3d9b595e6fe14f43cad24f604e4de4d6f5b6bf97
fishman123456 <den9152@yandex.ru>
2023-01-18 05:09:43 +0700

    1
    1

--- Layout_WF_void.cs
+++ Layout_WF_void.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.LayerManager;
 using Autodesk.AutoCAD.Runtime;
 
 using System;
@@ -11,10 +12,10 @@ using System.Threading.Tasks;
 
 namespace autokad_test_dll_15_01_2023
 {
-   public class Layout_WF_void
+    public class Layout_WF_void
     {
-         [CommandMethod("Lay_WF_Renum")]
-      //public  List<String> list_layout { get; set; }
+        [CommandMethod("Lay_WF_Renum")]
+        //public  List<String> list_layout { get; set; }
 
 
         public static void Cmd_Lay_Renum()
@@ -32,33 +33,25 @@ namespace autokad_test_dll_15_01_2023
             {
                 using (Transaction trans = db.TransactionManager.StartTransaction())
                 {
-
-
                     DBDictionary layoutDict = trans.GetObject(db.LayoutDictionaryId, OpenMode.ForRead) as DBDictionary;
 
                     foreach (DBDictionaryEntry layoutEntry in layoutDict)
                     {
-
                         Layout layout = trans.GetObject((ObjectId)layoutEntry.Value, OpenMode.ForRead) as Layout;
                         layout.UpgradeOpen();
-                       
                         list_layout.Add(layout.LayoutName.ToString());
-
                         //код для изменения имён листов-------
                         //https://stackoverflow.com/questions/72216779/renaming-layout-tab-autocad-c-sharp-net
                         //oldName = layout.LayoutName;
                         //newName = "blad" + layout.TabOrder.ToString("00");
-
-
                         //if (oldName != newName)
                         //{
                         //    if (layout.ModelType != model)
                         //    {
                         //        LayoutManager.Current.RenameLayout(oldName, newName);
-
                         //    }
                         //}
-                    }  
+                    }
                     trans.Commit();
                 }
             }
@@ -67,5 +60,12 @@ namespace autokad_test_dll_15_01_2023
                 edt.WriteMessage("\nError >> " + ex.Message);
             }
         }
+        public void list_met_listbox1()
+        {
+           
+
+
+        }
     }
+}
 }
