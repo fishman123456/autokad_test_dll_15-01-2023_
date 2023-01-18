6b2b705dc18b62418d52e3242b5d08da5ada8f67
fishman123456 <den9152@yandex.ru>
2023-01-19 01:49:37 +0700

    добавил класс Class_List

---
+++ Class_List.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autokad_test_dll_15_01_2023
+{
+    internal class Class_List
+    {
+        public List<string> list;
+        public string list_pol;
+
+        
+    }
+}
